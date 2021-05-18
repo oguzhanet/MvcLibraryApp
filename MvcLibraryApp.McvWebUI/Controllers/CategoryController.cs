af40ec354c92704f6be9ca6c86356ee5721b89c8
oğuzhan cengiz <ozancengiz898@gmail.com>
2021-05-18 12:55:14 +0300

    18.05.2021

--- MvcLibraryApp.McvWebUI/Controllers/CategoryController.cs
+++ MvcLibraryApp.McvWebUI/Controllers/CategoryController.cs
@@ -3,15 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MvcLibraryApp.McvWebUI.Models.Entity;
 
 namespace MvcLibraryApp.McvWebUI.Controllers
 {
     public class CategoryController : Controller
     {
         // GET: Category
+        LibraryAppDbEntities db = new LibraryAppDbEntities();
         public ActionResult Index()
         {
+            var result = db.Categories.ToList();
+            return View(result);
+        }
+
+        [HttpGet]
+        public ActionResult Add()
+        {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Add(Categories categories)
+        {
+            db.Categories.Add(categories);
+            db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
