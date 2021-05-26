331e99c58cc8d23bcb714e4816f96c28f97e5203
oğuzhan cengiz <ozancengiz898@gmail.com>
2021-05-26 15:11:41 +0300

    26.05.2021

---
+++ MvcLibraryApp.McvWebUI/Controllers/BorrowedBookController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcLibraryApp.McvWebUI.Models.Entity;
+
+namespace MvcLibraryApp.McvWebUI.Controllers
+{
+    public class BorrowedBookController : Controller
+    {
+        // GET: BorrowedBook
+        LibraryAppDbEntities db = new LibraryAppDbEntities();
+        public ActionResult Index()
+        {
+            var result = db.Movements.ToList();
+            return View(result);
+        }
+
+        [HttpGet]
+        public ActionResult Lend()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Lend(Movements movements)
+        {
+            db.Movements.Add(movements);
+            db.SaveChanges();
+            return View();
+        }
+    }
+}
