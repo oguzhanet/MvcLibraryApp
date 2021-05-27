9c01eef90187e111c62b3f6d22becbfee3d93e01
oğuzhan cengiz <ozancengiz898@gmail.com>
2021-05-27 16:04:41 +0300

    27.05.2021

--- MvcLibraryApp.McvWebUI/Controllers/OperationController.cs
+++ MvcLibraryApp.McvWebUI/Controllers/OperationController.cs
@@ -3,16 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MvcLibraryApp.McvWebUI.Models.Entity;
 
 namespace MvcLibraryApp.McvWebUI.Controllers
 {
     public class OperationController : Controller
     {
         // GET: Operation
+        LibraryAppDbEntities db = new LibraryAppDbEntities();
         public ActionResult Index()
         {
-
-            return View();
+            var result = db.Movements.Where(x => x.Status == true).ToList();
+            return View(result);
         }
     }
 }
