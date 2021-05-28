714ecbd6c466e39b7cba8d37b14f16095d810245
oğuzhan cengiz <ozancengiz898@gmail.com>
2021-05-28 16:01:29 +0300

    28.05.2021

--- MvcLibraryApp.McvWebUI/Controllers/ShowcaseController.cs
+++ MvcLibraryApp.McvWebUI/Controllers/ShowcaseController.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MvcLibraryApp.McvWebUI.Models.Entity;
 
 namespace MvcLibraryApp.McvWebUI.Controllers
 {
     public class ShowcaseController : Controller
     {
         // GET: Showcase
+        LibraryAppDbEntities db = new LibraryAppDbEntities();
         public ActionResult Index()
         {
-            return View();
+            var result = db.Books.ToList();
+            return View(result);
         }
     }
 }
