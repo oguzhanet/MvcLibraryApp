bdd71142c7b3fd52216f64820718561c5c3d416f
oğuzhan cengiz <ozancengiz898@gmail.com>
2021-05-30 15:59:09 +0300

    30.05.2021

--- MvcLibraryApp.McvWebUI/Controllers/StatisticsController.cs
+++ MvcLibraryApp.McvWebUI/Controllers/StatisticsController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -12,6 +13,33 @@ namespace MvcLibraryApp.McvWebUI.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult Air()
+        {
+            return View();
+        }
+
+        public ActionResult AirCard()
+        {
+            return View();
+        }
+
+        public ActionResult Gallery()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Upload(HttpPostedFileBase file)
+        {
+            if (file.ContentLength >0)
+            {
+                string result = Path.Combine(Server.MapPath("~/Templates/web2/resimler/"),
+                    Path.GetFileName(file.FileName));
+                file.SaveAs(result);
+            }
+            return RedirectToAction("Gallery");
         }
     }
 }
