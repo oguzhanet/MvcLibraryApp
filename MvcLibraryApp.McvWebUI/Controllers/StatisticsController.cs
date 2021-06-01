1b9049ecfd43974ff5aa28908da93c346841fa76
oğuzhan cengiz <ozancengiz898@gmail.com>
2021-06-01 21:32:37 +0300

    01.06.2021

--- MvcLibraryApp.McvWebUI/Controllers/StatisticsController.cs
+++ MvcLibraryApp.McvWebUI/Controllers/StatisticsController.cs
@@ -55,5 +55,48 @@ namespace MvcLibraryApp.McvWebUI.Controllers
             }
             return RedirectToAction("Gallery");
         }
+
+        public ActionResult LinqCard()
+        {
+            var result1 = db.Books.Count();
+            ViewBag.result1 = result1;
+
+            var result2 = db.Members.Count();
+            ViewBag.result2 = result2;
+
+            var result3 = db.Penalties.Sum(x => x.Money) * 25 / 100;
+            ViewBag.result3 = result3;
+
+            var result4 = db.Books.Where(x => x.Status == false).Count();
+            ViewBag.result4 = result4;
+
+            var result5 = db.Categories.Count();
+            ViewBag.result5 = result5;
+
+            var result6 = db.EnFazlaKitapAlanUye().FirstOrDefault();
+            ViewBag.result6 = result6;
+
+            var result7 = db.EnFazlaOkunanKitap().FirstOrDefault();
+            ViewBag.result7 = result7;
+
+            var result8 = db.EnFazlaKitapYazar().FirstOrDefault();
+            ViewBag.result8 = result8;
+
+            //var result9 = db.Books.GroupBy(x => x.PublishingHouse).OrderByDescending(y => y.Count()).Select(z =>
+            //      new { z.Key }).FirstOrDefault();
+            var result9 = db.EnFazlaKitapYayınevi().FirstOrDefault();
+            ViewBag.result9 = result9;
+
+            var result10 = db.EnFazlaKİtapVerenPersonel().FirstOrDefault();
+            ViewBag.result10 = result10;
+
+            var result11 = db.Contacts.Count();
+            ViewBag.result11 = result11;
+
+            var result12 = db.Movements.Where(x => x.DateOfIssue == DateTime.Today).Count();
+            ViewBag.result12 = result12;
+
+            return View();
+        }
     }
 }
--- MvcLibraryApp.McvWebUI/Models/Entity/Model1.Context.cs
+++ MvcLibraryApp.McvWebUI/Models/Entity/Model1.Context.cs
@@ -12,6 +12,8 @@ namespace MvcLibraryApp.McvWebUI.Models.Entity
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Infrastructure;
+    using System.Data.Entity.Core.Objects;
+    using System.Linq;
     
     public partial class LibraryAppDbEntities : DbContext
     {
@@ -36,5 +38,30 @@ namespace MvcLibraryApp.McvWebUI.Models.Entity
         public virtual DbSet<Writers> Writers { get; set; }
         public virtual DbSet<Abouts> Abouts { get; set; }
         public virtual DbSet<Contacts> Contacts { get; set; }
+    
+        public virtual ObjectResult<string> EnFazlaKitapYazar()
+        {
+            return ((IObjectContextAdapter)this).ObjectContext.ExecuteFunction<string>("EnFazlaKitapYazar");
+        }
+    
+        public virtual ObjectResult<string> EnFazlaKitapYayınevi()
+        {
+            return ((IObjectContextAdapter)this).ObjectContext.ExecuteFunction<string>("EnFazlaKitapYayınevi");
+        }
+    
+        public virtual ObjectResult<string> EnFazlaKitapAlanUye()
+        {
+            return ((IObjectContextAdapter)this).ObjectContext.ExecuteFunction<string>("EnFazlaKitapAlanUye");
+        }
+    
+        public virtual ObjectResult<string> EnFazlaKİtapVerenPersonel()
+        {
+            return ((IObjectContextAdapter)this).ObjectContext.ExecuteFunction<string>("EnFazlaKİtapVerenPersonel");
+        }
+    
+        public virtual ObjectResult<string> EnFazlaOkunanKitap()
+        {
+            return ((IObjectContextAdapter)this).ObjectContext.ExecuteFunction<string>("EnFazlaOkunanKitap");
+        }
     }
 }
