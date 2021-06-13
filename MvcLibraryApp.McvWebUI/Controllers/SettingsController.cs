7df7588f9d1ac0bf836dfeb080894d7c06ba94ca
oğuzhan cengiz <ozancengiz898@gmail.com>
2021-06-13 16:37:46 +0300

    13.06.2021

---
+++ MvcLibraryApp.McvWebUI/Controllers/SettingsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcLibraryApp.McvWebUI.Models.Entity;
+
+namespace MvcLibraryApp.McvWebUI.Controllers
+{
+    public class SettingsController : Controller
+    {
+        // GET: Settings
+        LibraryAppDbEntities db = new LibraryAppDbEntities();
+
+        public ActionResult Index()
+        {
+            var member = db.AdminLogins.ToList();
+            return View(member);
+        }
+    }
+}
